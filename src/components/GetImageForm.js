6060032cfea69d0361d6e2ade8f193d6badf9f68
Andrew Gibson <andrewgib14@gmail.com>
2017-09-19 14:44:02 -0500

    form rendering  button not submiting

--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react'
-import GetImageButton from
+import GetImageButton from './GetImageButton'
+import ImageDisplay from './ImageDisplay'
 import axios from 'axios';
-const API_KEY = "kEJ4QOWD4c8A3bbAPcDorbuQaH29a3dCjf83LfGt";
-let url = "https://api.nasa.gov/planetary/apod?";
-let urlQuery = "api_key=";
 
 export default class GetImageForm extends Component {
     constructor() {
         super();
-        this.state = {};
+        this.state = {
+            camera: "",
+            rover: "",
+            sol: ""
+            
+        }
     }
-
-    componentWillMount() {
-        axios.get(url + urlQuery + API_KEY).then((response) => {
-            // setState({})
-            console.log(response);
+    
+    componentWillUpdate() {
+        let cam = this.state.camera;
+        let rove = this.state.rover;
+        let num = this.state.sol;
+        const API_KEY = "kEJ4QOWD4c8A3bbAPcDorbuQaH29a3dCjf83LfGt";
+        let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&${urlQuery}${API_KEY}`;
+        let urlQuery = "api_key=";
+        axios.get(url).then((response) => {
+            this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
         }).catch((err) => {
             console.log(err);
         })
+    }
+
+    handelRover = e => {
+        this.setState({rover : e.target.value});
+
+    }
+
+    handleCamera = e => {
+        this.setState({camera: e.target.value});
+
     }
 
     render() {
@@ -40,7 +58,6 @@ export default class GetImageForm extends Component {
                 <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value} />
                 <GetImageButton />
                 <ImageDisplay />
-
             </div>
         )
     }
