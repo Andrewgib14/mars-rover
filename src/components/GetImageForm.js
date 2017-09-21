2aef837cd658db11be2288781baa5bc64d42a23d
Andrew Gibson <andrewgib14@gmail.com>
2017-09-21 10:28:37 -0500

    button submiting and imaged appearing

--- src/components/GetImageForm.js
+++ src/components/GetImageForm.js
@@ -7,35 +7,36 @@ export default class GetImageForm extends Component {
     constructor() {
         super();
         this.state = {
-            camera: "",
-            rover: "",
+            camera: "FHAZ",
+            rover: "Curiosity",
+            images: [],
             sol: ""
-            
         }
     }
     
-    componentWillUpdate() {
-        let cam = this.state.camera;
-        let rove = this.state.rover;
-        let num = this.state.sol;
-        const API_KEY = "kEJ4QOWD4c8A3bbAPcDorbuQaH29a3dCjf83LfGt";
-        let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rove}/photos?sol=${num}&camera=${cam}&${urlQuery}${API_KEY}`;
-        let urlQuery = "api_key=";
-        axios.get(url).then((response) => {
-            this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol});
-        }).catch((err) => {
-            console.log(err);
-        })
-    }
+    
 
-    handelRover = e => {
+    handleRover = e => {
         this.setState({rover : e.target.value});
-
     }
 
     handleCamera = e => {
         this.setState({camera: e.target.value});
+    }
 
+    handleSol = e => {
+        this.setState({sol: e.target.value});
+    }
+    handleSubmit = e => {
+            let {camera, rover, sol} = this.state;
+            const API_KEY = "kEJ4QOWD4c8A3bbAPcDorbuQaH29a3dCjf83LfGt";
+            let urlQuery = "api_key=";
+            let url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&${urlQuery}${API_KEY}`;
+            axios.get(url).then((response) => {
+                this.setState({images:response.data.photos});
+            }).catch((err) => {
+                console.log(err);
+            })
     }
 
     render() {
@@ -43,21 +44,21 @@ export default class GetImageForm extends Component {
         return (
             <div>
                 <label htmlFor="rover">Rover</label>
-                <select onChange={this.handleRover} id="rover" value={this.state.value}>
+                <select onChange={this.handleRover} id="rover" value={this.state.rover}>
                     <option value="Curiosity">Curiosity</option>
                     <option value="Opportunity">Opportunity</option>
                     <option value="Spirit">Spirt</option>
                 </select>
                 <label htmlFor="camera">Camera Type</label>
-                <select onChange={this.handleCamera} id="rover" value={this.state.value}>
+                <select onChange={this.handleCamera} id="rover" value={this.state.camera}>
                     <option value="fhaz">FHAZ (Front Hazard)</option>
                     <option value="rhaz">RHAZ (Rear Hazard)</option>
                     <option value="navcam">NAVCAM (Navigation Cam)</option>
                 </select>
                 <label htmlFor="sol">Martian Sol: 1000-2000</label>
-                <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.value} />
-                <GetImageButton />
-                <ImageDisplay />
+                <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.sol} />
+                <GetImageButton handleClick = {this.handleSubmit}/>
+                <ImageDisplay images = {this.state.images}/>
             </div>
         )
     }
--- src/components/ImageDisplay.js
+++ src/components/ImageDisplay.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
-export default () => {
-    return(
+export default (props) => {
+    let images = props.images.map((image, index)=>{
+        console.log('image: ', image);
+        
+        return(
+            <div key = {index}>
+                <img src={image["img_src"]}alt="" />
+            </div>
+        )
+    });
+    console.log(images);
+
+    return  (
         <div>
+            {images}
         </div>
     )
 }
